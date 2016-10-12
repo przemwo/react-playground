91974769c7794d871c854974dab6172c63d6cbe0
przemwo <przemwo@o2.pl>
2016-10-12 17:50:58 +0200

    first component

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Temp = () => {
+  return (
+    <h1>Hello from React!</h1>
+  );
+};
+
+ReactDOM.render(
+  <Temp />,
+  document.getElementById('app')
+);
