658602599791106d3aef7ddc869174bcd79e15b8
przemwo <przemwo@o2.pl>
2016-11-02 17:05:09 +0100

    egghead start using react - completed

---
+++ src/tutorial01/App10.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      val: 0
+    }
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.setState({
+      val: this.state.val + 1
+    });
+  }
+
+  componentWillMount() {
+    console.log('mounting');
+    this.setState({
+      m: 2
+    });
+  }
+
+  componentDidMount() {
+    console.log('mounted');
+    console.log(ReactDOM.findDOMNode(this));
+    this.inc = setInterval(this.update, 500);
+  }
+
+  componentWillUnmount() {
+    console.log('unmounting');
+    clearInterval(this.inc);
+
+  }
+
+  render() {
+    console.log('rendering');
+    return(
+      <button onClick={this.update}>{this.state.val * this.state.m}</button>
+    );
+  }
+}
+
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+    this.mount = this.mount.bind(this);
+    this.unmount = this.unmount.bind(this);
+  }
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'));
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
+  render() {
+    return(
+      <div>
+        <button onClick={this.mount}>Mount</button>
+        <button onClick={this.unmount}>Unmount</button>
+        <div id="a"></div>
+      </div>
+    );
+  }
+}
+
+export default Wrapper;
---
+++ src/tutorial01/readme.md
@@ -0,0 +1,2 @@
+# Start Using React to Build Web Applications
+**[Egghead.io tutorial](https://egghead.io/courses/react-fundamentals)**
