a3dd8aa7a403f0094f4d989b7797a740aa67270d
przemwo <przemwo@o2.pl>
2016-11-04 17:12:16 +0100

    react doc: state and lifecycle

--- src/App.js
+++ src/App.js
@@ -1,45 +1,30 @@
 import React from 'react';
-import { Router, Route, Link, browserHistory } from 'react-router';
-
-// const Home = () => <div><h1>Home</h1><Links /></div>;
-
-class Home extends React.Component {
-  componentWillMount() {
-    this.context.router.setRouteLeaveHook(
-      this.props.route,
-      this.routeWillLeave
-    );
-  }
-  routeWillLeave(nextLocation) {
-    console.log(`Leaving home for ${nextLocation.pathname}`);
-  }
-  render() {
-    return(
-      <div><h1>Home</h1><Links /></div>
-    );
-  }
-}
-
-Home.contextTypes = { router: React.PropTypes.object.isRequired };
-
-const About = () => <div><h1>About</h1><Links /></div>;
-
-const Links = () => {
-  return(
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-    </nav>
-  );
-}
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      date: new Date()
+    };
+    this.tick = this.tick.bind(this);
+  }
+  componentDidMount() {
+    this.timerID = setInterval(this.tick, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState((prevState, props) => ({
+      date: new Date()
+    }));
+  }
   render() {
     return(
-      <Router history={ browserHistory }>
-        <Route path="/" component={Home}></Route>
-        <Route path="/about" component={About}></Route>
-      </Router>
+      <div>
+        <h1>Hello!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
     );
   }
 }
