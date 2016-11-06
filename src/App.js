f603f86c677a2f519759dad9f71d3be65b5dadb9
przemwo <przemwo@o2.pl>
2016-11-06 13:41:29 +0100

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
 
+const GreetUser = () => {
+  return(
+    <h1>Hello User!</h1>
+  );
+}
+const GreetGuest = () => {
+  return(
+    <h1>Hello Guest!</h1>
+  );
+}
+const Greet = (props) => {
+  if (props.isLoggedIn) {
+    return <GreetUser />
+  } else {
+    return <GreetGuest />
+  };
+}
+const LogInButton = (props) => {
+  return(
+    <button
+      onClick={props.logIn}>
+      Log in
+    </button>
+  );
+};
+
+const LogOutButton = (props) => {
+  return(
+    <button
+      onClick={props.logOut}>
+      Log out
+    </button>
+  );
+};
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      button: false
+      isLoggedIn: false
     };
-    this.toggle = this.toggle.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
   }
-  toggle() {
-    this.setState((prevState) => ({
-      button: !prevState.button
-    }));
+  logIn() {
+    this.setState({
+      isLoggedIn: true
+    });
+  }
+  logOut() {
+    this.setState({
+      isLoggedIn: false
+    });
   }
   render() {
+    let button = null;
+    //Use if else for test the condition and assing element to a variable
+    if(this.state.isLoggedIn) {
+      button = <LogOutButton logOut={this.logOut} />;
+    } else {
+      button = <LogInButton logIn={this.logIn} />;
+    }
     return(
       <div>
-        <h1>State is: {this.state.button.toString()}</h1>
-        <button
-          onClick={this.toggle}>
-          Click me!
-        </button>
+        <h2>User is {this.state.isLoggedIn ? 'currently' : 'not'} logged in</h2> {/*inline if-else condition operator*/}
+        <h4>{this.state.isLoggedIn && `I'm logged in!`}</h4> {/* condition using logical && operator: true && sth ==> sth, false && sth ==> false */}
+        <Greet isLoggedIn={this.state.isLoggedIn} />
+        {button}
       </div>
     );
   }
