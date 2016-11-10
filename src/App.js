511e98a7c09da862e660b0f0cae1bb9d7750cf69
przemwo <przemwo@o2.pl>
2016-11-10 15:25:19 +0100

    react docs: Thinking In React

--- src/App.js
+++ src/App.js
@@ -1,69 +1,133 @@
 import React from 'react';
 
-const FancyBorder = (props) => {
-  return(
-    <div style={{color: props.color}}>
-      {props.children}
-    </div>
-  );
-};
+const PRODUCTS = [
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+];
 
-const Dialog = (props) => {
-  return(
-    <FancyBorder color="blue">
-      <h1 className="dialog-title">
-        {props.title}
-      </h1>
-      <p className="dialog-message">
-        {props.message}
-      </p>
-      {props.children}
-    </FancyBorder>
-  );
-};
-
-class SignUpDialog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {login: ''};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSignUp = this.handleSignUp.bind(this);
-  }
-  handleChange(e) {
-    this.setState({
-      login: e.target.value
-    });
-  }
-  handleSignUp() {
-    console.log(`Welcome aboard, ${this.state.login}`);
-  }
+class ProductCategoryRow extends React.Component {
   render() {
+    return <tr><th colSpan="2">{this.props.category}</th></tr>;
+  }
+}
+
+class ProductRow extends React.Component {
+  render() {
+    const name = this.props.product.stocked ?
+      this.props.product.name :
+      <span style={{color: 'red'}}>
+        {this.props.product.name}
+      </span>;
     return(
-      <Dialog
-        title="Mars Exploration Program"
-        message="How should we refer to you?">
-        <input
-          type="text"
-          value={this.state.login}
-          onChange={this.handleChange}/>
-        <button
-          onClick={this.handleSignUp}>
-          Sign Me Up!
-        </button>
-      </Dialog>
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
     );
   }
 }
 
-const WelcomeDialog = () => {
-  return(
-    <div>
-      <Dialog
-        title="Welcome"
-        message="Hello stranger!" />
-      <SignUpDialog />
-    </div>
-  );
-};
+class ProductTable extends React.Component {
+  render() {
+    let rows = [];
+    let lastCategory = null;
+    this.props.products.forEach((product) => {
+      if(product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.isStockOnly)) {
+        return;
+      }
+      if(product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+    return(
+      <table>
+        <thead>
+          <tr>Name</tr>
+          <tr>Price</tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
 
-export default WelcomeDialog;
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange() {
+    this.props.onUserInput(
+      this.refs.filterTextInput.value,
+      this.refs.isStockOnlyInput.checked
+    );
+  }
+  render() {
+    return(
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          ref="filterTextInput"
+          onChange={this.handleChange} />
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.handleChange}
+            ref="isStockOnlyInput"
+            checked={this.props.isStockOnly} />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      isStockOnly: false
+    };
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+  handleUserInput(filterText, isStockOnly) {
+    this.setState({
+      filterText: filterText,
+      isStockOnly: isStockOnly
+    });
+  }
+  render() {
+    return(
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          onUserInput={this.handleUserInput}
+          isStockOnly={this.state.isStockOnly} />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          isStockOnly={this.state.isStockOnly} />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return(
+      <FilterableProductTable products={PRODUCTS} />
+    );
+  }
+}
+
+export default App;
