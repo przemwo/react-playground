ffec4ea1e759a35ddc75784518bc641d05dbf2fa
przemwo <przemwo@o2.pl>
2016-11-10 12:54:50 +0100

    react docs: Composition vs Inheritance 02

--- src/App.js
+++ src/App.js
@@ -8,13 +8,61 @@ const FancyBorder = (props) => {
   );
 };
 
+const Dialog = (props) => {
+  return(
+    <FancyBorder color="blue">
+      <h1 className="dialog-title">
+        {props.title}
+      </h1>
+      <p className="dialog-message">
+        {props.message}
+      </p>
+      {props.children}
+    </FancyBorder>
+  );
+};
+
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {login: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      login: e.target.value
+    });
+  }
+  handleSignUp() {
+    console.log(`Welcome aboard, ${this.state.login}`);
+  }
+  render() {
+    return(
+      <Dialog
+        title="Mars Exploration Program"
+        message="How should we refer to you?">
+        <input
+          type="text"
+          value={this.state.login}
+          onChange={this.handleChange}/>
+        <button
+          onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </Dialog>
+    );
+  }
+}
+
 const WelcomeDialog = () => {
   return(
-    <FancyBorder
-      color="red">
-      <h1>Welcome!</h1>
-      <p>Some text</p>
-    </FancyBorder>
+    <div>
+      <Dialog
+        title="Welcome"
+        message="Hello stranger!" />
+      <SignUpDialog />
+    </div>
   );
 };
 
