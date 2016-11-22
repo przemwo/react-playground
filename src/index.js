a27a75f6cd837bcfddac4950ad88e644ea4cbe5a
przemwo <przemwo@o2.pl>
2016-11-22 17:58:44 +0100

    dan abramov redux tutorial 05

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('app')
+// );
+
+export const counter = (state = 0, action) => {
+  
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,26 @@
 import expect from 'expect';
+import * as index from './index';
 
 describe('Our first test', () => {
   it('should pass', () => {
     expect(true).toEqual(true);
   });
+  it('counter', () => {
+    expect(
+      index.counter(0, { type: 'INCREMENT' })
+    ).toEqual(1);
+    expect(
+      index.counter(1, { type: 'INCREMENT' })
+    ).toEqual(2);
+    expect(
+      index.counter(1, { type: 'DECREMENT' })
+    ).toEqual(0);
+    expect(
+      index.counter(1, { type: 'UNKNOWN_ACTION' })
+    ).toEqual(1);
+    expect(
+      index.counter(undefined, {})
+    ).toEqual(0);
+  });
+  console.log('Passed!!!');
 });
