5334eefa51ad1746a189ef360add230e1719ece6
przemwo <przemwo@o2.pl>
2016-11-28 19:15:13 +0100

    dabramov redux tutorial 11

--- src/index.js
+++ src/index.js
@@ -1,52 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-
-// Reducer method
-export const counter = (state = 0, action) => {
+// todos reducer
+export const todos = (state = [], action) => {
   switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
+    case 'ADD_TODO':
+      return [...state, {id: action.id, text: action.text, completed: false}];
     default:
       return state;
   }
 };
-
-// React dump component
-const Counter = (props) => {
-  return(
-    <div>
-      <h1>{props.value}</h1>
-      <button onClick={props.onIncrement}>+</button>
-      <button onClick={props.onDecrement}>-</button>
-    </div>
-  );
-};
-
-// Create store
-const store = createStore(counter);
-
-// Create render method - shows current state
-const render = () => {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() =>
-      store.dispatch({
-        type: 'INCREMENT'
-      })}
-      onDecrement={() =>
-      store.dispatch({
-        type: 'DECREMENT'
-      })} />,
-    document.getElementById('app')
-  );
-};
-
-// Subscribe render method to every change of the state
-store.subscribe(render);
-
-// Initial call to render method
-render();
--- src/index.test.js
+++ src/index.test.js
@@ -1,26 +1,22 @@
 import expect from 'expect';
 import * as index from './index';
 
-describe('Our first test', () => {
+describe('Test todos reducer', () => {
   it('should pass', () => {
-    expect(true).toEqual(true);
-  });
-  it('counter', () => {
-    expect(
-      index.counter(0, { type: 'INCREMENT' })
-    ).toEqual(1);
-    expect(
-      index.counter(1, { type: 'INCREMENT' })
-    ).toEqual(2);
-    expect(
-      index.counter(1, { type: 'DECREMENT' })
-    ).toEqual(0);
-    expect(
-      index.counter(1, { type: 'UNKNOWN_ACTION' })
-    ).toEqual(1);
-    expect(
-      index.counter(undefined, {})
-    ).toEqual(0);
+    const stateBefore = [];
+    const action = {
+      type: 'ADD_TODO',
+      id: 0,
+      text: 'Learn Redux'
+    };
+    const stateAfter = [
+      {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+      }
+    ];
+    expect(index.todos(stateBefore, action)).toEqual(stateAfter);
   });
   console.log('Passed!!!');
 });
