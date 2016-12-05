b01b2150c70bcedba77dc28b87d665f3df916e14
przemwo <przemwo@o2.pl>
2016-12-05 15:37:50 +0100

    dabrmov redux tutorial 14 - composing reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,102 @@
-// todos reducer
-export const todos = (state = [], action) => {
+import { createStore } from 'redux';
+
+// todo reducer
+export const todo = (state, action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [...state, {id: action.id, text: action.text, completed: false}];
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if(state.id !== action.id) {
+        return state;
+      }
+      return {
+        ...state,
+        completed: !state.completed
+      };
     default:
       return state;
   }
 };
+
+// todos reducer
+export const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
+      // return [...state, {id: action.id, text: action.text, completed: false}];
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action));
+    default:
+      return state;
+  }
+};
+
+// visiblity filter reducer
+export const visiblityFilter = (state = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+// todoApp main reducer
+export const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(state.todos, action),
+    visiblityFilter: visiblityFilter(state.visiblityFilter, action)
+  };
+};
+
+//create store and pass top reducer to it
+const store = createStore(todoApp);
+
+console.log('Initial state:');
+console.log(JSON.stringify(store.getState(), null, 2));
+console.log('====================================');
+
+console.log('Add todo 0:');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+});
+console.log('Current state:');
+console.log(JSON.stringify(store.getState(), null, 2));
+console.log('====================================');
+
+console.log('Add todo 1:');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go shopping'
+});
+console.log('Current state:');
+console.log(JSON.stringify(store.getState(), null, 2));
+console.log('====================================');
+
+console.log('Toggle todo 1:');
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 1,
+});
+console.log('Current state:');
+console.log(JSON.stringify(store.getState(), null, 2));
+console.log('====================================');
+
+console.log('Change visiblityFilter to show completed:');
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED'
+});
+console.log('Current state:');
+console.log(JSON.stringify(store.getState(), null, 2));
+console.log('====================================');
--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,60 @@
 import expect from 'expect';
 import * as index from './index';
 
-describe('Test todos reducer', () => {
-  it('should pass', () => {
-    const stateBefore = [];
-    const action = {
-      type: 'ADD_TODO',
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
       id: 0,
-      text: 'Learn Redux'
-    };
-    const stateAfter = [
-      {
-        id: 0,
-        text: 'Learn Redux',
-        completed: false
-      }
-    ];
-    expect(index.todos(stateBefore, action)).toEqual(stateAfter);
-  });
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+  ];
+  expect(index.todos(stateBefore, action)).toEqual(stateAfter);
+};
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ];
+  expect(index.todos(stateBefore, action)).toEqual(stateAfter);
+};
+
+describe('Test ', () => {
+  it('testAddTodo should', testAddTodo);
+  // it('testToggleTodo should', testToggleTodo);
   console.log('Passed!!!');
 });
+
+testToggleTodo();
+console.log('testToggleTodo passed');
